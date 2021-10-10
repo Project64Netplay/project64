bca7957d29f49e29ad963d9a7382df2973432ec3
Nora Hanegan <norahanegan@gmail.com>
2021-10-09 20:31:12 -0400

    change default state for pause emulation when window is not active

--- Source/Project64/Settings/UISettings.cpp
+++ Source/Project64/Settings/UISettings.cpp
@@ -25,7 +25,8 @@ void RegisterUISettings (void)
     // Settings location
     AddUISetting(Setting_PluginPageFirst, new CSettingTypeApplication("Settings", "Plugin Page First", false));
     AddUISetting(Setting_DisableScrSaver, new CSettingTypeApplication("Settings", "Disable Screen Saver", (uint32_t)true));
-    AddUISetting(Setting_AutoSleep, new CSettingTypeApplication("Settings", "Auto Sleep", (uint32_t)true));
+
+    AddUISetting(Setting_AutoSleep, new CSettingTypeApplication("Settings", "Auto Sleep", (uint32_t)false));
     AddUISetting(Setting_AutoFullscreen, new CSettingTypeApplication("Settings", "Auto Full Screen", (uint32_t)false));
 
     // RDB settings
